fa0d9345c1eac1d6cb147963a58b294e6cc83da5
jose alcantara <jose_luis967@hotmail.com>
2022-03-03 22:47:09 -0300

    Se agregan avances a proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header';
+import Search from './components/Search';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App text-center'>
+        <Header/>
+        <Search/>
+    <div className="pokemon-container">
     </div>
+    <nav className="pagination" aria-label="...">
+        <ul className="pagination">
+          <li className="page-item" id="previous">
+            <a className="page-link" href="#" tabindex="-1">Anterior</a>
+          </li>
+          <li className="page-item" id="next">
+            <a className="page-link" href="#">Siguiente</a>
+          </li>
+        </ul>
+      </nav>
+    <div id="spinner" className="spinner-border text-light" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    <script src="./main.js"></script>
+    </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div>
+            <nav class="nav justify-content-center">
+              <a class="nav-link active" href="./public/index.hmtl"><h1>Pokédex</h1></a>
+            </nav>
+        </div>
+    )
+
+}
+export default Header;
